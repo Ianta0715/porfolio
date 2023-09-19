ded49265bee2078216760adead2281efc8387fb0
Ian Tarquini <iantarquini4@gmail.com>
2023-09-19 09:22:54 -0300

    corregida las rutas de las apps

--- src/portfolio/pages/PortfolioInitialpage.jsx
+++ src/portfolio/pages/PortfolioInitialpage.jsx
@@ -29,7 +29,7 @@ export const PortfolioInitialpage = () => {
                     <div className="card-body">
                         <h5 className="card-title">Gif App</h5>
                     <p className="card-text">this is an App that has an &lt;input&gt; where you put the first thing that come's to your mind and it will show you diferent gifs of it. </p>
-                        <a href=" https://iantarquini15.github.io/heroApp/" target='_blank'  className="btn btn-primary">Visit</a>
+                        <a href=" https://iantarquini15.github.io/gifApp/" target='_blank'  className="btn btn-primary">Visit</a>
                     </div>
             </div>
             <div className="card" style={{ width: '18rem', margin: '2rem' }}>
@@ -38,7 +38,7 @@ export const PortfolioInitialpage = () => {
                 <div className="card-body">
                     <h5 className="card-title">Heros App</h5>
                     <p className="card-text">In this App there are diferent types of &lt;Routes&gt; (privates and publics),just by login in you can see all the app,enjoy it.</p>
-                        <a href="https://iantarquini15.github.io/heroApp/" target='_blank' className="btn btn-primary">Visit</a>
+                        <a href="https://iantarquini15.github.io/heroApp/#/marvel" target='_blank' className="btn btn-primary">Visit</a>
                     </div>
                     
                 </div>
