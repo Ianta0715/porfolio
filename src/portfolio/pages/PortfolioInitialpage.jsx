376e85e864085084ccd2cf82afac7f47ac0f2c89
Ian Tarquini <iantarquini4@gmail.com>
2023-09-19 09:28:48 -0300

    cambiado por error

--- src/portfolio/pages/PortfolioInitialpage.jsx
+++ src/portfolio/pages/PortfolioInitialpage.jsx
@@ -29,7 +29,7 @@ export const PortfolioInitialpage = () => {
                     <div className="card-body">
                         <h5 className="card-title">Gif App</h5>
                     <p className="card-text">this is an App that has an &lt;input&gt; where you put the first thing that come's to your mind and it will show you diferent gifs of it. </p>
-                        <a href=" https://iantarquini15.github.io/gifApp/" target='_blank'  className="btn btn-primary">Visit</a>
+                        <a href=" https://iantarquini15.github.io/heroApp/" target='_blank'  className="btn btn-primary">Visit</a>
                     </div>
             </div>
             <div className="card" style={{ width: '18rem', margin: '2rem' }}>
